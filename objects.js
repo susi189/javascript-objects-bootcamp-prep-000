c744f69645464208a44684e4c95257a56cd0e1d3
Susanna Brumm <susanna.brumm@gmail.com>
2018-02-12 01:49:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { artistName: songTitle};
+var playlist = { u2: 'One'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({},playlist, {[artistName]: songTitle});
