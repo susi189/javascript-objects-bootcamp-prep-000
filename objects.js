743a8963ed0029c5137d72ea2e3b3e532954b1ca
Susanna Brumm <susanna.brumm@gmail.com>
2018-02-12 01:33:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { u2: "With or Without you", Sting: "Shape of My Heart"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({},playlist, {[artistName]:'songTitle'})
+  return Object.assign({},playlist, {[artistName]: songTitle})
 }
